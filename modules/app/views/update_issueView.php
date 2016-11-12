b0c0cad0c281034e4c206b7da18d2c1bc4cac3b0
hieptm96 <hiep1996tb@gmail.com>
2016-11-12 16:16:40 +0700

    Update database config and init model, view

---
+++ modules/app/views/update_issueView.php
@@ -0,0 +1,19 @@
+
+
+<!-- page content -->
+<div class="right_col" role="main">
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Tables <small>Some examples to get you started</small></h3>
+      </div>
+      
+    </div>
+
+    <div class="clearfix"></div>
+
+    
+  </div>
+</div>
+<!-- /page content -->
+
